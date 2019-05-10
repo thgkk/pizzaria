3fd21c2544908d5571c31cde3da256ad597e65c5
thgkk <43996425+thgkk@users.noreply.github.com>
2019-05-10 14:51:15 -0300

    versao 1.2

--- t1/pizzeria.c
+++ t1/pizzeria.c
@@ -9,17 +9,31 @@
 #include <semaphore.h>
 
 queue_t* fila;
+
 sem_t garconsDisponiveis;
+
+sem_t pizzaiolosDisponiveis;
+
 pthread_mutex_t mtxPegarMesa;
+
 int mesasDisponiveis;
+
+int fornoDisponivel;
+
 void pizzeria_init(int tam_forno, int n_pizzaiolos, int n_mesas, int n_garcons, int tam_deck, int n_grupos) {
   pthread_mutex_init(&mtxPegarMesa,NULL);
 
   mesasDisponiveis = n_mesas;
 
+  fornoDisponivel = tam_forno;
+
+  sem_init(&pizzaiolosDisponiveis, 0 ,n_pizzaiolos);
+
   sem_init(&garconsDisponiveis, 0, n_garcons);
 
   queue_init(fila,tam_deck);
+
+
 }
 
 void pizzeria_close() {
@@ -29,6 +43,10 @@ void pizzeria_destroy() {
 }
 
 void pizza_assada(pizza_t* pizza) {
+  sem_wait(&pizzaiolosDisponiveis);
+  pizzaiolo_retirar_forno(pizza);
+  fornoDisponivel++;
+  sem_post(&pizzaiolosDisponiveis);
 }
 
 int pegar_mesas(int tam_grupo) {
@@ -64,7 +82,18 @@ void garcom_chamar() {
 }
 
 void fazer_pedido(pedido_t* pedido) {
+  queue_push_back(fila, (void*)pedido);
 
+  sem_wait(&pizzaiolosDisponiveis);
+  pizza_t* pizza = pizzaiolo_montar_pizza(pedido);
+  sem_post(&pizzaiolosDisponiveis);
+
+  if(fornoDisponivel > 0){
+    sem_wait(&pizzaiolosDisponiveis);
+    pizzaiolo_colocar_forno(pizza);
+    fornoDisponivel--;
+    sem_post(&pizzaiolosDisponiveis);
+  }
 }
 
 int pizza_pegar_fatia(pizza_t* pizza) {
