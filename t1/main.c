5854d200c789797a5cbc20aa7d9846d769fa39e8
thgkk <43996425+thgkk@users.noreply.github.com>
2019-05-12 18:50:38 -0300

    t1 v1.3

--- t1/main.c
+++ t1/main.c
@@ -8,10 +8,11 @@
 #include "helper.h"
 
 
+
 int main(int argc, char** argv) {
     int tam_forno = 1, n_pizzaiolos = 1, n_mesas = 10, n_garcons = 1,
         tam_deck = 0, n_grupos = 0, segs_execucao = 10;
-    
+
     if (argc < 7) {
         printf("Faltaram argumentos!\n"
                "Uso:%s tam_forno n_pizzaiolos n_mesas n_garcons "
@@ -24,19 +25,21 @@ int main(int argc, char** argv) {
     n_garcons    = atoi(argv[4]);
     tam_deck     = atoi(argv[5]);
     n_grupos     = atoi(argv[6]);
-    if (argc > 7) 
+    if (argc > 7)
         segs_execucao = atoi(argv[7]);
 
-   
+
     helper_init(tam_forno, n_pizzaiolos, n_mesas, n_garcons, tam_deck, n_grupos);
     pizzeria_init(tam_forno, n_pizzaiolos, n_mesas, n_garcons, tam_deck, n_grupos);
     pizzeria_open();
 
+    
+
     printf("Executando simulação por %d segundos\n", segs_execucao);
     sleep(segs_execucao);
     printf("Passados %d segundos, fechando pizzaria\n", segs_execucao);
-    
-    
+
+
     pizzeria_close();
     pizzeria_destroy();
     helper_destroy();
--- t1/pizzeria.c
+++ t1/pizzeria.c
@@ -8,94 +8,147 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-queue_t* fila;
+queue_t* smartDeck;
 
-sem_t garconsDisponiveis;
+sem_t semMesasDisponiveis, semGarconsDisponiveis, semPizzaiolosDisponiveis, semFornoDisponivel;
 
-sem_t pizzaiolosDisponiveis;
+pthread_mutex_t mtxPegarMesa, mtxPa, mtxPegador;
 
-pthread_mutex_t mtxPegarMesa;
+pthread_t *pizzailos, *garcons;
 
-int mesasDisponiveis;
-
-int fornoDisponivel;
+int pizzariaAberta;
 
 void pizzeria_init(int tam_forno, int n_pizzaiolos, int n_mesas, int n_garcons, int tam_deck, int n_grupos) {
+
+
+
+  pizzariaAberta = 1;
+
   pthread_mutex_init(&mtxPegarMesa,NULL);
 
-  mesasDisponiveis = n_mesas;
+  pthread_mutex_init(&mtxPa, NULL);
 
-  fornoDisponivel = tam_forno;
+  pthread_mutex_init(&mtxPegador, NULL);
 
-  sem_init(&pizzaiolosDisponiveis, 0 ,n_pizzaiolos);
+  //mesasDisponiveis = n_mesas;
 
-  sem_init(&garconsDisponiveis, 0, n_garcons);
+  sem_init(&semFornoDisponivel, 0, tam_forno);
 
-  queue_init(fila,tam_deck);
+  sem_init(&semMesasDisponiveis, 0, n_mesas);
+
+  sem_init(&semPizzaiolosDisponiveis, 0, n_pizzaiolos);
+
+  sem_init(&semGarconsDisponiveis, 0, n_garcons);
+
+  queue_init(smartDeck,tam_deck);
 
 
 }
 
 void pizzeria_close() {
+  pizzariaAberta = 0;
 }
 
 void pizzeria_destroy() {
+  pthread_mutex_destroy(&mtxPa);
+  pthread_mutex_destroy(&mtxPegador);
+  pthread_mutex_destroy(&mtxPegarMesa);
+
+  sem_destroy(&semFornoDisponivel);
+  sem_destroy(&semMesasDisponiveis);
+  sem_destroy(&semPizzaiolosDisponiveis);
+  sem_destroy(&semGarconsDisponiveis);
 }
 
 void pizza_assada(pizza_t* pizza) {
-  sem_wait(&pizzaiolosDisponiveis);
+  pthread_mutex_lock(&mtxPa);
   pizzaiolo_retirar_forno(pizza);
-  fornoDisponivel++;
-  sem_post(&pizzaiolosDisponiveis);
+  pthread_mutex_unlock(&mtxPa);
+  sem_post(&semFornoDisponivel);
 }
 
 int pegar_mesas(int tam_grupo) {
-  pthread_mutex_lock(&mtxPegarMesa);
+  /*pthread_mutex_lock(&mtxPegarMesa);
 
-
-  if (tam_grupo % 4 == 0){
+  if (tam_grupo % 4 == 0 && mesasDisponiveis>0){
     mesasDisponiveis -= tam_grupo/4;
 
-  } else {
+  }
+  if(tam_grupo % 4 != 0 && mesasDisponiveis>0) {
     mesasDisponiveis -= tam_grupo/4 + 1;
   }
   pthread_mutex_unlock(&mtxPegarMesa);
   return 0; //erro: não fui implementado (ainda)!
+}*/
+  if (!pizzariaAberta)
+    return -1;
+
+  if(tam_grupo % 4 == 0) {
+    for(int i = 0; i<tam_grupo/4 ;i++){
+      sem_wait(&semMesasDisponiveis);
+    }
+    return 0;
+  }
+  else{
+    for(int i=0; i<tam_grupo/4 + 1; i++){
+      sem_wait(&semMesasDisponiveis);
+    }
+    return 0;
+  }
+
 }
 
 void garcom_tchau(int tam_grupo) {
-  pthread_mutex_lock(&mtxPegarMesa);
-
-  if (tam_grupo % 4 == 0){
-    mesasDisponiveis += tam_grupo/4;
-
-  } else {
-    mesasDisponiveis += tam_grupo/4 + 1;
+  if(tam_grupo % 4 == 0) {
+    for(int i = 0; i<tam_grupo/4 ;i++)
+      sem_post(&semMesasDisponiveis);
   }
-  pthread_mutex_unlock(&mtxPegarMesa);
+  else{
+    for(int i=0; i<tam_grupo/4 + 1; i++)
+      sem_post(&semMesasDisponiveis);
+  }
 }
 
 void garcom_chamar() {
-  sem_wait(&garconsDisponiveis);
-  sem_post(&garconsDisponiveis);
+  sem_wait(&semGarconsDisponiveis);
+  sem_post(&semGarconsDisponiveis);
 
+}
+
+void* preparar_pizza(void* a){
+  void* pedidot = queue_wait(smartDeck);
+  pizza_t* pizza = pizzaiolo_montar_pizza((pedido_t*)pedidot);
+
+  sem_wait(&semFornoDisponivel);
+  pthread_mutex_lock(&mtxPa);
+  pizzaiolo_colocar_forno(pizza);
+  pthread_mutex_unlock(&mtxPa);
+  pthread_exit(NULL);
 }
 
 void fazer_pedido(pedido_t* pedido) {
-  queue_push_back(fila, (void*)pedido);
-
-  sem_wait(&pizzaiolosDisponiveis);
+  queue_push_back(smartDeck, pedido);
+  /*
+  sem_wait(&semPizzaiolosDisponiveis);
   pizza_t* pizza = pizzaiolo_montar_pizza(pedido);
-  sem_post(&pizzaiolosDisponiveis);
+  sem_post(&semPizzaiolosDisponiveis);
 
-  if(fornoDisponivel > 0){
-    sem_wait(&pizzaiolosDisponiveis);
+  if(semFornoDisponivel > 0){
+    sem_wait(&semPizzaiolosDisponiveis);
     pizzaiolo_colocar_forno(pizza);
-    fornoDisponivel--;
-    sem_post(&pizzaiolosDisponiveis);
-  }
+    semFornoDisponivel--;
+    sem_post(&semPizzaiolosDisponiveis);
+  }*/
 }
 
 int pizza_pegar_fatia(pizza_t* pizza) {
-    return -1; // erro: não fui implementado (ainda)!
+  pthread_mutex_lock(&mtxPegador);
+  if(pizza->fatias > 0){
+    pizza->fatias--;
+    pthread_mutex_unlock(&mtxPegador);
+    return 0;
+  } else {
+    pthread_mutex_unlock(&mtxPegador);
+    return -1;
+  }
 }
